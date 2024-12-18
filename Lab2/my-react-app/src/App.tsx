71c310fc6beb3cc0283613e18538145a5fbff163
Mayochiki03 <aphisorn2003@gmail.com>
2024-12-18 15:41:10 +0700

    “TSX-Tailwind”

--- Lab2/my-react-app/src/App.tsx
+++ Lab2/my-react-app/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-
 const App = () => {
+  const msg = "สวัสดีดีตอนเย็น พวกเราชอบเรียนเขียนโค้ด";
+  const num1 = 500;
+  const isTest2 = false;
   return (
-    <div className="bg-blue-500 text-white p-4">
+    <body className="bg-blue-500 text-white p-4">
       <h1 className="text-2xl">สวัสดีครับ ยินดีต้อนรับ</h1>
-    </div>
+      <p style={{ color: "Red", fontSize: "24" }}>{msg}</p>
+      <p className="test1" >{((num1+3)^2)/4}</p>
+
+      <p className= {isTest2 ? "test1" : "test2"} > condition Rendering </p>
+    </body>
+
   );
 };
 
