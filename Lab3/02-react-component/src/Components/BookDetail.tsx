03e8c0e4976b4727f6d765b5f7bd65bd17574270
Mayochiki03 <aphisorn2003@gmail.com>
2025-01-22 13:29:14 +0700

    first steps

--- Lab3/02-react-component/src/Components/BookDetail.tsx
+++ Lab3/02-react-component/src/Components/BookDetail.tsx
@@ -1,7 +1,7 @@
 import { Star } from 'lucide-react';
 
 // TODO: Import Book Interface
-
+import Book from '../utils/Book';
 
 function Rating(rating: number) {
   return Array.from({ length: 5 }, (_, index) => (
@@ -28,18 +28,18 @@ function BookDetail({ book }: { book: Book }) {
   return (
     <div  className={`border rounded-lg p-4 m-2 shadow-md hover:shadow-xl transition-shadow w-64 flex flex-col ${getColorByRating(book.rating)}`}>
       <img
-        src="https://mp-static.se-ed.com/physical/cover/to35l6xefa680079jd4f/image/w0ibhw1u"
-        alt="ชายไร้สีกับปีแสวงบุญ"
+        src= {book.coverUrl}
+        alt={book.title}
         className="w-full h-80 object-cover rounded-t-lg mb-4"
       />
-      <h2 className="font-semibold mb-2">ชายไร้สีกับปีแสวงบุญ</h2>
-      <p className="text-gray-700 mb-1">โดย Haruki Murakami</p>
+      <h2 className="font-semibold mb-2">{book.title.length>25 ?   `${book.title.substring(0,25)}... `: book.title }</h2>
+      <p className="text-gray-700 mb-1">โดย {book.author}</p>
       <div className="mb-2">
-        {Rating(5)}
+        {Rating(book.rating)}
       </div>
       <div className="flex justify-between items-center mt-auto">
-        <span className="text-blue-600 font-medium">฿ 320</span>
-        <span className="text-gray-500 text-sm">300 หน้า</span>
+        <span className="text-blue-600 font-medium">฿ {book.price}</span>
+        <span className="text-gray-500 text-sm">{book.pages} หน้า</span>
       </div>
     </div>
   );
--- Lab3/02-react-component/src/Mainpage.tsx
+++ Lab3/02-react-component/src/Mainpage.tsx
@@ -1,23 +1,33 @@
 // TODO: Import Components ที่เกี่ยวข้องทั้งหมด
 
+import BookList from "./Components/BookList";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Welcome from "./Components/Welcome";
+
+
+
 function MainPage() {
 
     return (
       <div className="flex flex-col min-h-screen">
       
         {/* TODO: แสดง Header  */}
+      <Header/>
         
         <main className="flex-grow container mx-auto px-4 py-6">
   
   
           {/* TODO: แสดง Welcome */}
+          <Welcome/>
+          <BookList/>
           
           {/* TODO: แสดง BookList */}
           
           
         </main>
         
-        {/* TODO: แสดง Footer */}
+        <Footer/>
         
       </div>
     );
